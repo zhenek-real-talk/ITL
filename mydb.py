c46a394f7d3beb2b992ce7fab2f6288c2d6892d7
Yevhenii Zozulia <zhekazozulya99@gmail.com>
2023-12-18 12:14:48 +0000

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,24 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python 
+
+
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'innovative99'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE ITL")
+
+print("All Done!")
